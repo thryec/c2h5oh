9e2a4086d9b24362f94815e2647ec91714602216
thryec <thryec@gmail.com>
2022-06-03 15:02:32 +0800

    added deployment tests and mints tokens to team on deployment

--- test/nft.test.js
+++ test/nft.test.js
@@ -10,26 +10,38 @@ const TOTAL_SUPPLY = 400
 
 describe('Merkle Whitelist NFT', () => {
   let deployer
-  let whitelist1
-  let whitelist2
-  let whitelist3
+  let teamWallet1
+  let teamWallet2
+  let pubWhitelist1
+  let pubWhitelist2
+  let freeWhitelist1
+  let freeWhitelist2
   let unwhitelisted
 
-  let merkleTree
+  let pubMerkleTree
   let nft
   let proof
 
   const mintPrice = ethers.utils.parseEther('0.2')
 
   beforeEach(async () => {
-    ;[deployer, whitelist1, whitelist2, whitelist3, unwhitelisted] = await ethers.getSigners()
-    const whitelist = [whitelist1, whitelist2, whitelist3]
-    const leafNodes = whitelist.map((addr) => keccak256(addr.address))
-    merkleTree = new MerkleTree(leafNodes, keccak256, { sort: true })
-    const rootHash = merkleTree.getRoot()
+    ;[
+      deployer,
+      teamWallet1,
+      teamWallet2,
+      pubWhitelist1,
+      pubWhitelist2,
+      freeWhitelist1,
+      freeWhitelist2,
+      unwhitelisted,
+    ] = await ethers.getSigners()
+    const pubWhitelist = [pubWhitelist1, pubWhitelist2]
+    const pubLeafNodes = pubWhitelist.map((addr) => keccak256(addr.address))
+    pubMerkleTree = new MerkleTree(pubLeafNodes, keccak256, { sort: true })
+    const pubRootHash = pubMerkleTree.getRoot()
 
     const NFT = await ethers.getContractFactory('NFT')
-    nft = await NFT.deploy(rootHash)
+    nft = await NFT.deploy(pubRootHash)
     await nft.deployed()
   })
 
@@ -37,11 +49,20 @@ describe('Merkle Whitelist NFT', () => {
     it('sets contract deployer as owner of contract', async () => {
       expect(await nft.owner()).to.equal(deployer.address)
     })
+
     it('sets token name', async () => {
       expect(await nft.name()).to.equal(NAME)
     })
+
     it('sets token symbol', async () => {
       expect(await nft.symbol()).to.equal(SYMBOL)
+    })
+
+    it('mints 5 tokens to each team address', async () => {
+      expect(await nft.balanceOf(teamWallet1.address)).to.equal(5)
+      expect(await nft.balanceOf(teamWallet2.address)).to.equal(5)
+      expect(await nft.balanceOf(pubWhitelist1.address)).to.equal(5)
+      expect(await nft.balanceOf(pubWhitelist2.address)).to.equal(5)
     })
   })
 
@@ -55,12 +76,14 @@ describe('Merkle Whitelist NFT', () => {
   describe('Public Whitelist Mint', async () => {
     it('allows public whitelisted address to mint', async () => {
       const mintQty = 2
-      proof = merkleTree.getHexProof(keccak256(whitelist1.address))
-      await nft.connect(whitelist1).publicWhitelistMint(proof, mintQty, { value: mintPrice.mul(2) })
+      proof = pubMerkleTree.getHexProof(keccak256(pubWhitelist1.address))
+      await nft
+        .connect(pubWhitelist1)
+        .publicWhitelistMint(proof, mintQty, { value: mintPrice.mul(2) })
     })
 
     it('does not allow public non-whitelisted address to mint', async () => {
-      const badProof = merkleTree.getHexProof(keccak256(unwhitelisted.address))
+      const badProof = pubMerkleTree.getHexProof(keccak256(unwhitelisted.address))
       await expectRevert(
         nft.connect(unwhitelisted).publicWhitelistMint(badProof),
         'PublicMerkleWhitelist: Caller is not whitelisted',
@@ -72,6 +95,18 @@ describe('Merkle Whitelist NFT', () => {
 
   // describe('Free Whitelist Mint', (async) => {
   //   it('only allows contract owner to withdraw funds', async () => {})
+  // })
+
+  // describe('Reading Token Data', async () => {
+  //   it('Returns token IDs owned by user', async () => {})
+  //   it('Returns token URI by ID', async () => {})
+  //   it('Returns baseURI', async () => {})
+  //   it('Returns total supply of collection', async () => {})
+  // })
+
+  // describe('Updating Whitelists', async () => {
+  //   it('Successfully updates public mint whitelist', async () => {})
+  //   it('Successfully updates free mint whitelist', async () => {})
   // })
 
   // describe('Withdrawing Funds', (async) => {
